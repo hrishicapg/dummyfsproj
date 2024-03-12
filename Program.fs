b81b4d3f2a3a5f859222adeb5cfa2c661611b54d
hrishicapg <hrishikesh.hrishikesh@capgemini.com>
2024-03-12 11:58:14 +0530

    just setup the fucking debugger man!! i'm a genius

--- Program.fs
+++ Program.fs
@@ -13,14 +13,50 @@ open Serilog
 [<Literal>]
 let yamlFile = "/app/logix-ai.yml"
 
-let logixAIFile = new FileStream("./default-project.yml", FileMode.Open, FileAccess.Read)
-let stream = new StreamReader(logixAIFile)
-let deserializer = (new DeserializerBuilder()).WithNamingConvention(CamelCaseNamingConvention.Instance).Build()
-let yamlObject = deserializer.Deserialize(stream);
+let loadYAMLAndConvertToJSON filePath =
+    let file = new FileStream(filePath, FileMode.Open, FileAccess.Read)
+    let streamReader = new StreamReader(file)
+    let yamlText = File.ReadAllText(filePath)
+    let deserializer = (new DeserializerBuilder()).Build()
+    let yamlObject = deserializer.Deserialize<obj>(yamlText);
+    printf "%O\n\n" (streamReader.ReadToEnd())
+    let json = JsonConvert.SerializeObject(yamlObject, Formatting.Indented);
+    let jsonObject = JObject.Parse(json);
+    // // printf "%s" (jsonObject["name"].ToString())
+    // jsonObject.Property("kind").Remove()
+    printf "%O" jsonObject
+    jsonObject
 
-let json = JsonConvert.SerializeObject(yamlObject);
-let jsonObject = JObject.Parse(json);
-jsonObject.Property("kind").Remove()
+let findLatestEve dirPath=
+    let mutable latestEveVerison = None
+    if not(Directory.Exists(dirPath)) then
+        Log.Logger.Error("Could not find directoryPath {directoryPath}", dirPath)
+        None
+    else
+        let mutable files = Directory.GetFiles(dirPath, "*.yml")
+        files <-  Array.concat [files;  Directory.GetFiles(dirPath, "*.yaml") ]
+        for file in files do
+            let json = loadYAMLAndConvertToJSON file
+            if(json["kind"].ToString() = "eve-os-latest-version") then
+                latestEveVerison <- Some (json["version"].ToString())
+        latestEveVerison
+
+let iterateYamlFiles directoryPath =
+    if (Directory.Exists(directoryPath) = false) then
+        Log.Logger.Error("Could not find directoryPath {directoryPath}", directoryPath)
+        [||]
+    else
+        let mutable files = Directory.GetFiles(directoryPath, "*.yml")
+        files <-  Array.concat [files;  Directory.GetFiles(directoryPath, "*.yaml") ]
+        Array.map (fun elem -> loadYAMLAndConvertToJSON elem) files
+
+loadYAMLAndConvertToJSON "./default-project.yml" |> ignore
+// let jsonData = iterateYamlFiles "./"
+
+// jsonData |> Array.iter (fun x -> printf "%O" x)
+
+// printf "%O" (findLatestEve "./")
+
 
 // let js = new JsonSerializer();
 // let w = new StringWriter();
@@ -31,4 +67,4 @@ jsonObject.Property("kind").Remove()
 
 
 // Log.Logger.Information("File path: {jsonObject}", jsonObject)
-Log.Logger.Information("Data: {jsonObject}", jsonObject)
+// Log.Logger.Information("Data: {jsonObject}", jsonData)
