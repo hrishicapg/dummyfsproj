2608e7a72fb74072477c6c68ee217fae8882f2aa
hrishicapg <hrishikesh.hrishikesh@capgemini.com>
2024-04-26 12:56:16 +0530

    test cromn-job

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
 FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build-env
 WORKDIR /app
 
+RUN apt-get update && apt-get -y install cron
+
 # Copy everything
 COPY . ./
+COPY cron-job /etc/cron.d/cron-job
 # Restore as distinct layers
 RUN dotnet restore
 # Build and publish a release
--- Program.fs
+++ Program.fs
@@ -10,47 +10,70 @@ open Newtonsoft.Json.Linq
 open Serilog
 
 
-[<Literal>]
-let yamlFile = "/app/logix-ai.yml"
+// [<Literal>]
+// let yamlFile = "/app/logix-ai.yml"
 
-let loadYAMLAndConvertToJSON filePath =
-    let file = new FileStream(filePath, FileMode.Open, FileAccess.Read)
-    let streamReader = new StreamReader(file)
-    let yamlText = File.ReadAllText(filePath)
-    let deserializer = (new DeserializerBuilder()).Build()
-    let yamlObject = deserializer.Deserialize<obj>(yamlText);
-    printf "%O\n\n" (streamReader.ReadToEnd())
-    let json = JsonConvert.SerializeObject(yamlObject, Formatting.Indented);
-    let jsonObject = JObject.Parse(json);
-    // // printf "%s" (jsonObject["name"].ToString())
-    // jsonObject.Property("kind").Remove()
-    printf "%O" jsonObject
-    jsonObject
+// let convertBoolString(json: JObject) =
+//     let tokens = json.Children()
+//     for token in tokens do
+//         printf "%O\n" (token)
+//     let enumerator =tokens.GetEnumerator()
+//     for property in json do
+        // printf "%O: %O" property.Key property.Value
 
-let findLatestEve dirPath=
-    let mutable latestEveVerison = None
-    if not(Directory.Exists(dirPath)) then
-        Log.Logger.Error("Could not find directoryPath {directoryPath}", dirPath)
-        None
-    else
-        let mutable files = Directory.GetFiles(dirPath, "*.yml")
-        files <-  Array.concat [files;  Directory.GetFiles(dirPath, "*.yaml") ]
-        for file in files do
-            let json = loadYAMLAndConvertToJSON file
-            if(json["kind"].ToString() = "eve-os-latest-version") then
-                latestEveVerison <- Some (json["version"].ToString())
-        latestEveVerison
 
-let iterateYamlFiles directoryPath =
-    if (Directory.Exists(directoryPath) = false) then
-        Log.Logger.Error("Could not find directoryPath {directoryPath}", directoryPath)
-        [||]
-    else
-        let mutable files = Directory.GetFiles(directoryPath, "*.yml")
-        files <-  Array.concat [files;  Directory.GetFiles(directoryPath, "*.yaml") ]
-        Array.map (fun elem -> loadYAMLAndConvertToJSON elem) files
+    // json.Properties()
+    // |> Seq.iter (fun prop ->
+    //     match prop.Value with
+    //     | :? JValue as jv ->
+    //         match jv.Value with
+    //         | :? string as s when s.ToLower() = "true" || s.ToLower() = "false" -> prop.Value <- new JValue(true)
+    //         | _ -> ()
+    //     | _ -> ()
+    // )
+    // printf "%s" (System.Convert.ToString(json))
+    // json
 
-loadYAMLAndConvertToJSON "./default-project.yml" |> ignore
+// let loadYAMLAndConvertToJSON filePath =
+//     let file = new FileStream(filePath, FileMode.Open, FileAccess.Read)
+//     let streamReader = new StreamReader(file)
+//     // let yamlText = File.ReadAllText(filePath)
+//     // let deserializer = (new DeserializerBuilder()).Build()
+//     let yamlObject = (new Deserializer()).Deserialize(streamReader);
+//     // printf "%O\n\n" (streamReader.ReadToEnd())
+//     // let serializer = (new SerializerBuilder()).JsonCompatible().Build();
+//     let json = JsonConvert.SerializeObject(yamlObject);
+//     let jsonObject = JObject.Parse(json);
+//     convertBoolString jsonObject |> ignore
+//     // // printf "%s" (jsonObject["name"].ToString())
+//     // jsonObject.Property("kind").Remove()
+//     // printf "%O" jsonObject
+//     jsonObject
+
+// let findLatestEve dirPath=
+//     let mutable latestEveVerison = None
+//     if not(Directory.Exists(dirPath)) then
+//         Log.Logger.Error("Could not find directoryPath {directoryPath}", dirPath)
+//         None
+//     else
+//         let mutable files = Directory.GetFiles(dirPath, "*.yml")
+//         files <-  Array.concat [files;  Directory.GetFiles(dirPath, "*.yaml") ]
+//         for file in files do
+//             let json = loadYAMLAndConvertToJSON file
+//             if(json["kind"].ToString() = "eve-os-latest-version") then
+//                 latestEveVerison <- Some (json["version"].ToString())
+//         latestEveVerison
+
+// let iterateYamlFiles directoryPath =
+//     if (Directory.Exists(directoryPath) = false) then
+//         Log.Logger.Error("Could not find directoryPath {directoryPath}", directoryPath)
+//         [||]
+//     else
+//         let mutable files = Directory.GetFiles(directoryPath, "*.yml")
+//         files <-  Array.concat [files;  Directory.GetFiles(directoryPath, "*.yaml") ]
+//         Array.map (fun elem -> loadYAMLAndConvertToJSON elem) files
+// 
+// loadYAMLAndConvertToJSON "./default-project.yml" |> ignore
 // let jsonData = iterateYamlFiles "./"
 
 // jsonData |> Array.iter (fun x -> printf "%O" x)
@@ -68,3 +91,8 @@ loadYAMLAndConvertToJSON "./default-project.yml" |> ignore
 
 // Log.Logger.Information("File path: {jsonObject}", jsonObject)
 // Log.Logger.Information("Data: {jsonObject}", jsonData)
+
+let os = Environment.OSVersion; 
+let opSys = new OperatingSystem(os.Platform, os.Version); 
+
+printf "%O\n%O" os opSys
